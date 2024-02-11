0c114e81fc71895500572d8a4a904d347313fcfd
SunJiHoon <cangogi1@naver.com>
2024-02-11 17:13:04 +0900

    초기 설정

---
+++ LearnHub/src/main/java/AIMentor/LearnHub/LearnHubApplication.java
@@ -0,0 +1,13 @@
+package AIMentor.LearnHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHubApplication.class, args);
+	}
+
+}
---
+++ LearnHub/src/test/java/AIMentor/LearnHub/LearnHubApplicationTests.java
@@ -0,0 +1,13 @@
+package AIMentor.LearnHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
