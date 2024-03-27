9bdc45f3be41934b8adb7c3d431fea6526b98b80
SunJiHoon <cangogi1@naver.com>
2024-03-27 16:04:20 +0900

    general user를 위한 페이지 추가

---
+++ LearnHub/src/main/java/AIMentor/LearnHub/controller/GeneralUserController.java
@@ -0,0 +1,14 @@
+package AIMentor.LearnHub.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping(value = "/general/user")
+@Slf4j
+public class GeneralUserController {
+
+}
