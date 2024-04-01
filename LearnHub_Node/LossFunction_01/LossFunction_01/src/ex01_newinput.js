8ea2d0a0099bcad4439a3b2a07d263d2294e86b4
SunJiHoon <cangogi1@naver.com>
2024-04-01 09:58:37 +0900

    LossFunction 함수 리팩터링

--- LearnHub_Node/LossFunction_01/LossFunction_01/src/ex01_newinput.js
+++ LearnHub_Node/LossFunction_01/LossFunction_01/src/ex01_newinput.js
@@ -6,6 +6,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 export default function example() {
 	// Renderer
 	const canvas = document.querySelector('#three-canvas');
+	var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
 	const renderer = new THREE.WebGLRenderer({
 		canvas,
 		antialias: true
@@ -39,67 +41,42 @@ export default function example() {
 	// Controls
 	const controls = new OrbitControls(camera, renderer.domElement);
 	controls.enableDamping = true;
-	// controls.enableZoom = false;
-	// controls.maxDistance = 10;
-	// controls.minDistance = 3;
-	// controls.minPolarAngle = Math.PI / 4; // 45도
-	// controls.minPolarAngle = THREE.MathUtils.degToRad(45);
-	// controls.maxPolarAngle = THREE.MathUtils.degToRad(135);
-	// controls.target.set(2, 2, 2);
-	
-	// controls.autoRotate = true;
-	// controls.autoRotateSpeed = 50;
-
-
 
 	// Mesh
 	const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-
-
-
 	
 	// Get the input element and button element
-const objectCountInput = document.getElementById('object-count');
-const generateObjectsButton = document.getElementById('generate-objects-button');
-const clearButton = document.getElementById('generate-objects-clearButton');
-const fx_function = document.getElementById('fx_function');
-const x_start = document.getElementById('x_start');
-const x_end = document.getElementById('x_end');
+	const objectCountInput = document.getElementById('object-count');
+	const generateObjectsButton = document.getElementById('generate-objects-button');
+	const clearButton = document.getElementById('generate-objects-clearButton');
+	const fx_function = document.getElementById('fx_function');
+	const x_start = document.getElementById('x_start');
+	const x_end = document.getElementById('x_end');
 
-const fy_function = document.getElementById('fy_function');
-const y_start = document.getElementById('y_start');
-const y_end = document.getElementById('y_end');
+	const fy_function = document.getElementById('fy_function');
+	const y_start = document.getElementById('y_start');
+	const y_end = document.getElementById('y_end');
 
-function parseAndCreateFunction(expression) {
-    // 'return' 키워드를 추가하여 함수를 생성합니다.
-    return new Function('x', 'return ' + expression);
-}
+	function parseAndCreateFunction(expression) {
+    	// 'return' 키워드를 추가하여 함수를 생성합니다.
+    	return new Function('x', 'return ' + expression);
+	}
 
+	function handleClearButtonClick() {
+    	const meshesToRemove = [];
+    	scene.children.forEach(child => {
+        	if (child instanceof THREE.Mesh) {
+            	meshesToRemove.push(child);
+        	}
+    	});
 
-// // 수식 예시: "x * x + 2 * x - 7"
-// const expression = "x * x + 2 * x - 7";
-// const parsedFunction = parseAndCreateFunction(expression);
+    	meshesToRemove.forEach(mesh => {
+        	scene.remove(mesh);
+    	});
+	}
 
-// // 예시 값 x = 3일 때의 결과
-// const result = parsedFunction(3);
-// console.log(result); // 출력: 8
-
-
-function handleClearButtonClick() {
-    const meshesToRemove = [];
-    scene.children.forEach(child => {
-        if (child instanceof THREE.Mesh) {
-            meshesToRemove.push(child);
-        }
-    });
-
-    meshesToRemove.forEach(mesh => {
-        scene.remove(mesh);
-    });
-}
-
-// Add event listener to the button
-clearButton.addEventListener('click', handleClearButtonClick);
+	// Add event listener to the button
+	clearButton.addEventListener('click', handleClearButtonClick);
 
 
 	// Add event listener to the button
@@ -111,55 +88,66 @@ clearButton.addEventListener('click', handleClearButtonClick);
 		const parsedFunction_fy_function = parseAndCreateFunction(fy_function.value);
 
         // Generate objects based on the input number
-        for (let i = parseFloat(x_start.value); i < parseFloat(x_end.value); i += 0.1) {
-			for(let j = parseFloat(y_start.value); j < parseFloat(y_end.value); j+= 0.1){
+		let di = 0.1;
+		let dj = 0.1;
+        for (let i = parseFloat(x_start.value); i < parseFloat(x_end.value); i += di) {
+			for(let j = parseFloat(y_start.value); j < parseFloat(y_end.value); j+= dj){
+
 				console.log(i);
-				let material = new THREE.MeshStandardMaterial({
-					color: `rgb(
-						${ 50 + Math.floor(Math.random() * 205) },
-						${ 50 + Math.floor(Math.random() * 205) },
-						${ 50 + Math.floor(Math.random() * 205) }
-					)`
-				});
-				let mesh = new THREE.Mesh(geometry, material);
-				mesh.position.x = i;
-				mesh.position.z = j;
-				mesh.position.y = parsedFunction_fx_function(i) + parsedFunction_fy_function(j);
-				scene.add(mesh);
+
+				const geometry1 = new THREE.BufferGeometry();
+				const vertices1 = new Float32Array([
+					i, parsedFunction_fx_function(i) + parsedFunction_fy_function(j), j,  // 첫번째 점
+					i + (di/2), parsedFunction_fx_function(i + (di/2)) + parsedFunction_fy_function(j +(dj/2)), j +(dj/2),  // 두번째 점
+					i + di, parsedFunction_fx_function(i + di) + parsedFunction_fy_function(j), j, // 세번째 점
+				]);
+				geometry1.setAttribute('position', new THREE.BufferAttribute(vertices1, 3));
+				
+				const material1 = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }); // 양면 모두 표시하도록 side 설정
+				const mesh1 = new THREE.Mesh(geometry1, material1);
+				scene.add(mesh1);
+
+				const geometry2 = new THREE.BufferGeometry();
+				const vertices2 = new Float32Array([
+					i + di, parsedFunction_fx_function(i + di) + parsedFunction_fy_function(j), j,  // 첫번째 점
+					i + (di/2), parsedFunction_fx_function(i + (di/2)) + parsedFunction_fy_function(j +(dj/2)), j +(dj/2),  // 두번째 점
+					i + di, parsedFunction_fx_function(i + di) + parsedFunction_fy_function(j + dj), j + dj, // 세번째 점
+				]);
+				geometry2.setAttribute('position', new THREE.BufferAttribute(vertices2, 3));
+				
+				const material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide }); // 양면 모두 표시하도록 side 설정
+				const mesh2 = new THREE.Mesh(geometry2, material2);
+				scene.add(mesh2);
+
+
+				const geometry3 = new THREE.BufferGeometry();
+				const vertices3 = new Float32Array([
+					i + di, parsedFunction_fx_function(i + di) + parsedFunction_fy_function(j + dj), j + dj,  // 첫번째 점
+					i + (di/2), parsedFunction_fx_function(i + (di/2)) + parsedFunction_fy_function(j +(dj/2)), j +(dj/2),  // 두번째 점
+					i , parsedFunction_fx_function(i ) + parsedFunction_fy_function(j + dj), j + dj, // 세번째 점
+				]);
+				geometry3.setAttribute('position', new THREE.BufferAttribute(vertices3, 3));
+				
+				const material3 = new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide }); // 양면 모두 표시하도록 side 설정
+				const mesh3 = new THREE.Mesh(geometry3, material3);
+				scene.add(mesh3);
+
+				// const geometry4 = new THREE.BufferGeometry();
+				// const vertices4 = new Float32Array([
+				// 	i , parsedFunction_fx_function(i) + parsedFunction_fy_function(j + dj), j + dj,  // 첫번째 점
+				// 	i + (di/2), parsedFunction_fx_function(i + (di/2)) + parsedFunction_fy_function(j +(dj/2)), j +(dj/2),  // 두번째 점
+				// 	i , parsedFunction_fx_function(i) + parsedFunction_fy_function(j), j, // 세번째 점
+				// ]);
+				// geometry4.setAttribute('position', new THREE.BufferAttribute(vertices4, 3));
+				
+				// const material4 = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide }); // 양면 모두 표시하도록 side 설정
+				// const mesh4 = new THREE.Mesh(geometry4, material4);
+				// scene.add(mesh4);
+
+				
 			}
         }
 	});
-
-// // Add event listener to the button
-// generateObjectsButton.addEventListener('click', () => {
-//     // Get the number of objects from the input field
-//     const objectCount = parseInt(objectCountInput.value);
-
-//     // Check if the input is a valid number
-//     if (!isNaN(objectCount)) {
-//         // Remove existing objects from the scene
-// 		handleClearButtonClick();
-
-//         // Generate objects based on the input number
-//         for (let i = 0; i < objectCount; i++) {
-//             let material = new THREE.MeshStandardMaterial({
-//                 color: `rgb(
-//                     ${ 50 + Math.floor(Math.random() * 205) },
-//                     ${ 50 + Math.floor(Math.random() * 205) },
-//                     ${ 50 + Math.floor(Math.random() * 205) }
-//                 )`
-//             });
-//             let mesh = new THREE.Mesh(geometry, material);
-//             mesh.position.x = (Math.random() - 0.5) * 5;
-//             mesh.position.y = (Math.random() - 0.5) * 5;
-//             mesh.position.z = (Math.random() - 0.5) * 5;
-//             scene.add(mesh);
-//         }
-//     } else {
-//         // Notify the user if the input is not a valid number
-//         alert('Please enter a valid number.');
-//     }
-// });
 
 
 	// 그리기
