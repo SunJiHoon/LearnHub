0886a4c3d59f94955524816f1a170b9445289060
SunJiHoon <cangogi1@naver.com>
2024-04-01 19:26:40 +0900

    devLossFunction 추가

--- LearnHub_Node/LossFunction_01/LossFunction_01/src/ex01_newinput.js
+++ LearnHub_Node/LossFunction_01/LossFunction_01/src/ex01_newinput.js
@@ -49,6 +49,7 @@ export default function example() {
 	const objectCountInput = document.getElementById('object-count');
 	const generateObjectsButton = document.getElementById('generate-objects-button');
 	const clearButton = document.getElementById('generate-objects-clearButton');
+	
 	const fx_function = document.getElementById('fx_function');
 	const x_start = document.getElementById('x_start');
 	const x_end = document.getElementById('x_end');
@@ -57,6 +58,17 @@ export default function example() {
 	const y_start = document.getElementById('y_start');
 	const y_end = document.getElementById('y_end');
 
+	const dot_x = document.getElementById('dot_x');
+	const dot_y = document.getElementById('dot_y');
+	const dot_learning_rate = document.getElementById('dot_learning_rate');
+	const generateDotButton = document.getElementById('generate-dot-button');
+	const removeDotClearButton = document.getElementById('remove-dot-clearButton');
+
+
+	function numericalDifferentiation(f, x, h = 0.0001) {
+		return (f(x + h) - f(x)) / h;
+	}
+	
 	function parseAndCreateFunction(expression) {
     	// 'return' 키워드를 추가하여 함수를 생성합니다.
     	return new Function('x', 'return ' + expression);
@@ -74,9 +86,23 @@ export default function example() {
         	scene.remove(mesh);
     	});
 	}
+	function handleClearButtonClickSphere() {
+		const meshesToRemove = [];
+		scene.children.forEach(child => {
+			if (child instanceof THREE.Mesh && child.geometry instanceof THREE.SphereGeometry) {
+				// child가 Mesh이면서 geometry가 SphereGeometry인 경우에만 meshesToRemove 배열에 추가합니다.
+				meshesToRemove.push(child);
+			}
+		});
+	
+		meshesToRemove.forEach(mesh => {
+			scene.remove(mesh); // meshesToRemove 배열에 있는 모든 구를 scene에서 제거합니다.
+		});
+	}
 
 	// Add event listener to the button
 	clearButton.addEventListener('click', handleClearButtonClick);
+	removeDotClearButton.addEventListener('click', handleClearButtonClickSphere);
 
 
 	// Add event listener to the button
@@ -84,8 +110,8 @@ export default function example() {
 		console.log(fx_function.value);
         // Remove existing objects from the scene
 		handleClearButtonClick();
-		const parsedFunction_fx_function = parseAndCreateFunction(fx_function.value);
-		const parsedFunction_fy_function = parseAndCreateFunction(fy_function.value);
+		let parsedFunction_fx_function = parseAndCreateFunction(fx_function.value);
+		let parsedFunction_fy_function = parseAndCreateFunction(fy_function.value);
 
         // Generate objects based on the input number
 		let di = 0.1;
@@ -148,13 +174,65 @@ export default function example() {
 			}
         }
 	});
+	// Add event listener to the button
+	generateDotButton.addEventListener('click', () => {
+		console.log("현재 학습률 : " + dot_learning_rate.value);
+		
+		console.log(dot_x.value);
+		console.log(dot_y.value);
+
+    	// Geometry and Material
+		const sphereGeometry = new THREE.SphereGeometry(0.1, 32, 32);
+		const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+		const sphereDot = new THREE.Mesh(sphereGeometry, sphereMaterial);
+		sphereDot.position.x = dot_x.value;
+		sphereDot.position.z = dot_y.value;
+		let parsedFunction_fx_function = parseAndCreateFunction(fx_function.value);
+		let parsedFunction_fy_function = parseAndCreateFunction(fy_function.value);
+
+		sphereDot.position.y = parsedFunction_fx_function(dot_x.value) + parsedFunction_fy_function(dot_y.value);
+		// sphereDot.position.y = 0;
+		scene.add(sphereDot);
+	});
 
 
 	// 그리기
 	const clock = new THREE.Clock();
+	let lastExecutionTime = 0; // 마지막 작업 실행 시간
 
+	const moveMesh = [];
 	function draw() {
-		const delta = clock.getDelta();
+
+		// const delta = clock.getDelta();
+		const elapsedTime = clock.getElapsedTime(); // 경과된 총 시간을 초 단위로 가져옴
+		if (moveMesh.length >= 1){
+			console.log("가나다");
+		}
+    	// 10초마다 특정 작업 수행
+    	if (elapsedTime - lastExecutionTime >= 10) {
+        	// 여기서 10초마다 실행할 작업 수행
+        	console.log("10초가 경과했습니다. dot 갱신을 진행합니다.");
+        	// 마지막 작업 실행 시간 갱신
+			let parsedFunction_fx_function = parseAndCreateFunction(fx_function.value);
+			let parsedFunction_fy_function = parseAndCreateFunction(fy_function.value);
+	
+			const meshesTomove = [];
+			scene.children.forEach(child => {
+				if (child instanceof THREE.Mesh && child.geometry instanceof THREE.SphereGeometry) {
+					// child가 Mesh이면서 geometry가 SphereGeometry인 경우에만 meshesToRemove 배열에 추가합니다.
+					meshesTomove.push(child);
+				}
+			});
+		
+			meshesTomove.forEach(mesh => {
+				// scene.remove(mesh); // meshesToRemove 배열에 있는 모든 구를 scene에서 제거합니다.
+				mesh.position.x = Math.random() * 10 - 5; // x 좌표를 -5에서 5 사이의 랜덤 값으로 설정
+				mesh.position.z = Math.random() * 10 - 5; // z 좌표를 -5에서 5 사이의 랜덤 값으로 설정
+				mesh.position.y = parsedFunction_fx_function(mesh.position.x) + parsedFunction_fy_function(mesh.position.z);
+			});
+	
+        	lastExecutionTime = elapsedTime;
+    	}
 
 		controls.update();
 
