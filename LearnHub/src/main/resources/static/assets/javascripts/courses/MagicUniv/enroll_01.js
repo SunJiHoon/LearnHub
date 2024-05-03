3788ee6c7a3319f70f74c756e9a437b27b3df37f
SunJiHoon <cangogi1@naver.com>
2024-05-03 15:45:13 +0900

    enroll 서사 사진 완성 및 그림 제작

--- LearnHub/src/main/resources/static/assets/javascripts/courses/MagicUniv/enroll_01.js
+++ LearnHub/src/main/resources/static/assets/javascripts/courses/MagicUniv/enroll_01.js
@@ -1,31 +1,30 @@
 const images =
     [
-        "/assets/images/ImagePresent/WoodRou_edu_picture.jpg",
-        "/assets/images/ImagePresent/WoodRou_edu_picture.jpg",
-        "/assets/images/ImagePresent/WoodRou_edu_picture.jpg",
+        "/assets/images/MagicUniv/enroll_welcome.jpg",
+        "/assets/images/MagicUniv/enroll_welcome.jpg",
+        "/assets/images/MagicUniv/enroll_welcome.jpg",
 
-        "/assets/images/ImagePresent/WoodRou_edu_picture_blackAndWhite.jpg",
-        "/assets/images/ImagePresent/WoodRou_edu_picture_blackAndWhite.jpg",
-        "/assets/images/ImagePresent/WoodRou_edu_picture_blackAndWhite.jpg",
-        "/assets/images/ImagePresent/WoodRou_edu_picture_blackAndWhite.jpg",
-        "/assets/images/ImagePresent/WoodRou_edu_picture_blackAndWhite.jpg",
+        "/assets/images/MagicUniv/enroll_math.jpg",
+        "/assets/images/MagicUniv/enroll_math.jpg",
+        "/assets/images/MagicUniv/enroll_math.jpg",
+        "/assets/images/MagicUniv/enroll_math.jpg",
 
-        "/assets/images/ImagePresent/WoodRou_edu_picture_blackAndWhite.jpg",
-        "/assets/images/ImagePresent/WoodRou_edu_picture_blackAndWhite.jpg",
+        "/assets/images/MagicUniv/enroll_welcome.jpg",
+        "/assets/images/MagicUniv/enroll_welcome.jpg",
     ]; // 전환할 이미지들의 경로
 const texts = [
-    "마법학교의 강의실 안에서, 우드로우 교수님이 이미지 자료의 표현과 처리에 대해 설명하고 있었다.",
-    " \"이미지 자료의 표현은 마법적인 방식으로 이루어져 있어요,\" 우드로우 교수님이 말했다. ",
-    "\"마치 마법의 색채와 빛의 반응을 이용하여 이미지를 그리듯이, 우리는 이미지를 행렬로 표현할 수 있어요.\"",
+    "\"마법학교에 입학한 것을 환영하네! ",
+    "자네는 이제 우드로우 교수님의 수업을 듣게 될 걸세. 여기서는 마법의 힘을 이용하여 인공지능 수학을 배울 수 있을 거야.\"",
+    "\"우드로우 교수님은 최고의 마법학자이자 수학자로, 그의 강의는 놀라운 지식과 신비한 주문을 배울 수 있는 곳이야. \"",
 
-    "그러던 중 갑자기 강의실 안의 모든 색이 사라졌다. 학생들은 놀란 표정으로 주변을 둘러보았다. ",
-    "그 순간, 우드로우 교수님이 눈에 띄게 실망한 표정으로 강의를 중단했다.",
-    "\"아니요, 또 색깔이 사라졌어요. 이번에는 악마 마법사 마사의 짓이겠군요.\"",
-    "학생들은 주변을 살펴보았다. ",
-    "강의실 안에는 흑백으로 변한 이미지들이 여기저기 흩어져 있었다. ",
+    "\"인공지능 수학은 마법과 기계의 합작이라고 생각하면 돼.\"" ,
+    "우리는 마법의 원리를 이용하여 컴퓨터를 통해 문제를 해결하고 예측하는 법을 배울 거야.\"" ,
+    "\"수업에서는 다양한 주제를 다루며, 실제로 마법을 이용하여 데이터를 분석하고 패턴을 찾는 연습을 하게 될 거야. " ,
+    "또한 인공지능 알고리즘과 마법적인 방법을 결합하여 문제를 해결하는 능력을 키울 수 있을 거야.\"\n" ,
 
-    "우드로우 교수님이 머리를 긁적거리며 말했다. ",
-    "\"우리가 해야 할 일은 각자의 이미지에 올바른 색깔을 되찾는 것이에요.\" ",
+    "\"우드로우 교수님의 강의를 통해 인공지능 수학의 신비를 탐험하며, 자네는 놀라운 마법의 세계를 열어볼 수 있을 거야.\"" +
+    "\"지금부터 흥미진진한 여정이 시작되었네!\"",
+
 ]; // 이미지에 대응하는 텍스트
 
 let currentImageIndex = 0;
