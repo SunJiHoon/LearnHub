5763202674ff266b7d8b1c1f25388a78a8af9011
SunJiHoon <cangogi1@naver.com>
2024-06-04 13:37:30 +0900

    3 - 2 경향성과 예측 단원 서사 틀 구현

---
+++ LearnHub_Node/js/src/TendencyAndPrediction/TrendLine_01.js
@@ -0,0 +1,19 @@
+import { generateCutscene } from "../../lib/cutscene";
+
+const images = [
+    "/assets/images/TextMaterials/magicBook.jfif",
+    "/assets/images/TextMaterials/magicBook.jfif",
+    // "/assets/images/TextMaterials/magicBook.jfif",
+    // "/assets/images/TextMaterials/magicBook.jfif",
+]; // 전환할 이미지들의 경로
+
+const texts = [
+    "추세선과 예측",
+    "시작",
+    // "\"이 책은 그저 읽는 것이 아니라, 그 내용을 마법으로 저장할 수 있는 마법력을 지니고 있지.\"\n",
+    // "\"지금부터 단어 의미나 이미지들을 마법서에 저장하고, 그것들을 마법처럼 활용하는 법을 배워보자.\"\n",
+]; // 이미지에 대응하는 텍스트
+
+document.addEventListener('DOMContentLoaded', () => {
+	generateCutscene(document.getElementById('talkInterface'), images, texts);
+});
