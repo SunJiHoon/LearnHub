789660f3173393ec1909ad0eaf071bfb0e281f6e
SunJiHoon <cangogi1@naver.com>
2024-06-08 16:30:54 +0900

    4-1 구현

--- LearnHub_Node/js/src/ReasonableDecision/Case_03.js
+++ LearnHub_Node/js/src/ReasonableDecision/Case_03.js
@@ -3,15 +3,13 @@ import { generateCutscene } from "../../lib/cutscene";
 const images = [
     "/assets/images/TextMaterials/magicBook.jfif",
     "/assets/images/TextMaterials/magicBook.jfif",
-    // "/assets/images/TextMaterials/magicBook.jfif",
-    // "/assets/images/TextMaterials/magicBook.jfif",
+    "/assets/images/TextMaterials/magicBook.jfif",
 ]; // 전환할 이미지들의 경로
 
 const texts = [
-    "인공지능의 합리적 의사 결정 사례",
-    "끝",
-    // "\"이 책은 그저 읽는 것이 아니라, 그 내용을 마법으로 저장할 수 있는 마법력을 지니고 있지.\"\n",
-    // "\"지금부터 단어 의미나 이미지들을 마법서에 저장하고, 그것들을 마법처럼 활용하는 법을 배워보자.\"\n",
+    "오늘 우리가 다룬 주제 중에 특히 바둑 게임에 대해 어떻게 생각하셨나요? ",
+    "인공지능이 바둑에서 최적의 수를 결정하는 방식이 정말 흥미롭지 않았나요?",
+    "바둑은 전략과 패턴 인식을 요구하는 게임으로, 인공지능이 바둑에서 우리에게 어떻게 최적화된 의사 결정을 보여주는지에 대한 좋은 벤치마크가 될 수 있습니다.",
 ]; // 이미지에 대응하는 텍스트
 
 document.addEventListener('DOMContentLoaded', () => {
